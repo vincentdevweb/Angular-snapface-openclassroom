16dae72dc3d799b713a76bb33ce77e5da1b42ed5
Vincent BOLLOT <vincent.bollot@gmail.com>
2023-03-15 21:04:46 +0100

    pipeUpeercase/lowercase/titlecase et date + local language pour date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}

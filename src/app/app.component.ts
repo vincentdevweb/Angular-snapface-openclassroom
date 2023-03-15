d46ef6d5cf01182b80fce4082c98e2dc81745749
Vincent BOLLOT <vincent.bollot@gmail.com>
2023-03-15 20:48:42 +0100

    ngstyle / ngif/ ngfor/ ngclass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,58 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  FaceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Archibal',
-      'Van Grenier ! Personnage récurent des épisodes de JDG',
-      'https://media.tenor.com/W7lNExv_HP0AAAAC/jdg-genie.gif',
-      new Date(),
-      0
-    );
-    this.myOtherSnap = new FaceSnap(
-      'Mia',
-      'Les chatons ont maintenant 2 mois. Ils atteignent chaque jour de nouveaux sommets et atteignent de nouveaux horizons. Faire une sieste sur le toit d\'une maison n\'est plus une option pour Mia',
-      'https://i.ytimg.com/vi/N8NgLWDzKdM/maxresdefault.jpg',
-      new Date(),
-      0
-    );
-    this.myLastSnap = new FaceSnap(
-      'Happy Pallas cat',
-      'C\'est un beau Pallas qui donne du bonheur',
-      'https://media.tenor.com/HSjA9S4R4osAAAAM/manul.gif',
-      new Date(),
-      0
-    );
+    this.FaceSnaps = [
+      {
+        title: 'Archibal',
+        description: 'Van Grenier ! Personnage récurent des épisodes de JDG',
+        imageUrl: 'https://media.tenor.com/W7lNExv_HP0AAAAC/jdg-genie.gif',
+        createdDate: new Date(),
+        snaps: 250,
+        location: 'Bretagne'
+      },
+      {
+        title: 'Mia',
+        description: 'Les chatons ont maintenant 2 mois. Ils atteignent chaque jour de nouveaux sommets et atteignent de nouveaux horizons. Faire une sieste sur le toit d\'une maison n\'est plus une option pour Mia',
+        imageUrl: 'https://i.ytimg.com/vi/N8NgLWDzKdM/maxresdefault.jpg',
+        createdDate: new Date(),
+        snaps: 150,
+        location: 'Montagne'
+      },
+      {
+        title: 'Happy Pallas cat',
+        description: 'C\'est un beau Pallas qui donne du bonheur',
+        imageUrl: 'https://media.tenor.com/HSjA9S4R4osAAAAM/manul.gif',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
+
   }
+
+  // ngOnInit(): void {
+  //   this.mySnap = new FaceSnap(
+  //     'Archibal',
+  //     'Van Grenier ! Personnage récurent des épisodes de JDG',
+  //     'https://media.tenor.com/W7lNExv_HP0AAAAC/jdg-genie.gif',
+  //     new Date(),
+  //     0
+  //   );
+  //   this.myOtherSnap = new FaceSnap(
+  //     'Mia',
+  //     'Les chatons ont maintenant 2 mois. Ils atteignent chaque jour de nouveaux sommets et atteignent de nouveaux horizons. Faire une sieste sur le toit d\'une maison n\'est plus une option pour Mia',
+  //     'https://i.ytimg.com/vi/N8NgLWDzKdM/maxresdefault.jpg',
+  //     new Date(),
+  //     0
+  //   );
+  //   this.myLastSnap = new FaceSnap(
+  //     'Happy Pallas cat',
+  //     'C\'est un beau Pallas qui donne du bonheur',
+  //     'https://media.tenor.com/HSjA9S4R4osAAAAM/manul.gif',
+  //     new Date(),
+  //     0
+  //   );
+  // }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,8 +1,15 @@
 export class FaceSnap {
-    constructor(public title: string,
-        public description: string,
-        public imageUrl: string,
-        public createdDate: Date,
-        public snaps: number) {
-    }
+    title!: string;
+    description!: string;
+    imageUrl!: string;
+    createdDate!: Date;
+    snaps!: number;
+    location?: string;
+
+    // constructor(public title: string,
+    //     public description: string,
+    //     public imageUrl: string,
+    //     public createdDate: Date,
+    //     public snaps: number) {
+    // }
 }

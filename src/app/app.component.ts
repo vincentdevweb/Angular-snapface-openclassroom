91acc5ebcb6cc4d434fe0a5f8814ee4e899ee2d3
Vincent BOLLOT <vincent.bollot@gmail.com>
2023-03-18 20:26:45 +0100

    observable et fuite de memoire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable, of } from 'rxjs';
+import { concatMap, mergeMap, delay, exhaustMap, map, switchMap, take, tap, filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
+  ngOnInit() {
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      concatMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
 
-  // ngOnInit(): void {
-  //   this.mySnap = new FaceSnap(
-  //     'Archibal',
-  //     'Van Grenier ! Personnage récurent des épisodes de JDG',
-  //     'https://media.tenor.com/W7lNExv_HP0AAAAC/jdg-genie.gif',
-  //     new Date(),
-  //     0
-  //   );
-  //   this.myOtherSnap = new FaceSnap(
-  //     'Mia',
-  //     'Les chatons ont maintenant 2 mois. Ils atteignent chaque jour de nouveaux sommets et atteignent de nouveaux horizons. Faire une sieste sur le toit d\'une maison n\'est plus une option pour Mia',
-  //     'https://i.ytimg.com/vi/N8NgLWDzKdM/maxresdefault.jpg',
-  //     new Date(),
-  //     0
-  //   );
-  //   this.myLastSnap = new FaceSnap(
-  //     'Happy Pallas cat',
-  //     'C\'est un beau Pallas qui donne du bonheur',
-  //     'https://media.tenor.com/HSjA9S4R4osAAAAM/manul.gif',
-  //     new Date(),
-  //     0
-  //   );
-  // }
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
+    );
+  }
+
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
+  }
 }
